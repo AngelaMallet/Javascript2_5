76296afaf1d42a8059f69eaa44b7203350e761b1
AngelaMallet <angelamallet@gmail.com>
2018-10-11 10:08:12 +0200

    modif avec bonus

--- js.js
+++ js.js
@@ -1,27 +1,5 @@
 // j'ouvre le document ou j'appelle l'id de l'image1. quand je passe sur l'image avec
 // la souris je sollicite une action
-document.getElementById('image1').addEventListener('mouseover', function(){
-  // la fonction est : je modifie en direct l'url de l'image
-  document.getElementById('image1').src = 'images/image1_2.jpg';
-}
-)
-document.getElementById('image2').addEventListener('mouseover', function(){
-  // la fonction est : je modifie en direct l'url de l'image
-  document.getElementById('image2').src = 'images/image2_2.jpg';
-}
-)
-document.getElementById('image3').addEventListener('mouseover', function(){
-  // la fonction est : je modifie en direct l'url de l'image
-  document.getElementById('image3').src = 'images/image3_2.jpg';
-}
-)
-document.getElementById('image4').addEventListener('mouseover', function(){
-  // la fonction est : je modifie en direct l'url de l'image
-  document.getElementById('image4').src = 'images/image4_2.jpg';
-}
-)
-document.getElementById('image5').addEventListener('mouseover', function(){
-  // la fonction est : je modifie en direct l'url de l'image
-  document.getElementById('image5').src = 'images/image5_2.jpg';
-}
-)
+function change(Fabin){
+  Fabin.src='images/'+Fabin.id+'_2.jpg';
+};
